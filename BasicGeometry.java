ade0b6203396877770d934d09600336ebc70a93e
zoperpogi <zoperpogi@gmail.com>
2021-03-16 18:25:12 +0800

    Initial Commit

---
+++ BasicGeometry.java
@@ -0,0 +1,15 @@
+public class BasicGeometry {
+
+    
+    public static void main(String[] args) {
+        
+         Formulas fmla = new Formulas();
+
+    }
+    
+}
+
+class Formulas{
+    
+    
+}
